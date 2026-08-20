896a80c35c1143a375d271ebdbc6e112dcee228e
Benjamin Rush <bnjmnrsh@gmail.com>
2026-08-20 01:53:00 +0100

    docs: correct updater integration examples

--- tests/PlatformContractTest.php
+++ tests/PlatformContractTest.php
@@ -17,4 +17,33 @@ final class PlatformContractTest extends TestCase {
 
 		self::assertSame( '*', $composer['require']['ext-zip'] ?? null );
 	}
+
+	public function testReadmeExamplesMatchTheBootstrapAndSelectedRuntimeContract(): void {
+		$readme = (string) file_get_contents( dirname( __DIR__ ) . '/README.md' );
+
+		self::assertStringContainsString(
+			'composer require ran/wp-github-release-updater:^2.0@beta',
+			$readme
+		);
+		self::assertStringContainsString(
+			'At declaration time, only the returned bootstrap facade is',
+			$readme
+		);
+		self::assertStringContainsString(
+			'a callback at `PHP_INT_MAX`',
+			$readme
+		);
+		self::assertStringContainsString(
+			'use RAN\WPGitHubReleaseUpdater\V1\WordPress\ReleaseCandidatePreflight;',
+			$readme
+		);
+		self::assertStringContainsString(
+			'providerRepositoryId: \'123456789\'',
+			$readme
+		);
+		self::assertStringContainsString(
+			'providerRepositoryId: \'987654321\'',
+			$readme
+		);
+	}
 }
